f72b24c6f02c9a7024f2d4f7f9cce6fcfd67e8c9
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-22 23:49:31 -0500

    add loop for get data

--- Main.java
+++ Main.java
@@ -8,14 +8,14 @@ class Main {
     System.out.println("iphone 1");
     iphone.setSize("5,4");
     iphone.setColor("red");
-    System.out.println(iphone.brand+" "+iphone.getSize()+" "+ iphone.getColor());
+    System.out.println(iphone.brand+"\r\n"+iphone.getSize()+"\r\n"+ iphone.getColor()+"\r\n"+ iphone.getCableType());
 
     line.printSeparatorLine();
 
     System.out.println("iphone 2");
     iphone2.setSize("6,4");
     iphone2.setColor("white");
-    System.out.println(iphone.brand+" "+iphone2.getSize()+" "+ iphone2.getColor());
+    System.out.println(iphone.brand+"\r\n"+iphone2.getSize()+"\r\n"+ iphone2.getColor()+"\r\n"+ iphone2.getCableType());
 
     line.printSeparatorLine();
 
@@ -24,7 +24,42 @@ class Main {
     xiaomi.setSize("6,7");
     xiaomi.setColor("purple");
     xiaomi.setCableType("usb");
+    System.out.println(xiaomi.brand+"\r\n"+xiaomi.getSize()+"\r\n"+ xiaomi.getColor()+"\r\n"+ xiaomi.getCableType());
 
     line.printSeparatorLine();
+
+    while(true) {
+      ScanData sc = new ScanData();
+      String msg; String person;
+
+      System.out.println("want to text?");
+      System.out.println("1. send a message");
+      System.out.println("2. send a message for someone");
+      System.out.println("3. EXIT.");
+      
+      String select = sc.scanData();
+      int parseSelect = Integer.parseInt(select);
+
+      if(parseSelect == 1) {
+        System.out.print("write your text: ");
+        msg = sc.scanData();
+
+        xiaomi.setMessage(msg);
+        xiaomi.sendMsg(xiaomi.getMessage());
+      } else if (parseSelect == 2) {
+        System.out.print("write your text: ");
+        msg = sc.scanData();
+
+        System.out.print("write the person: ");
+        person = sc.scanData();
+        
+        xiaomi.setMessage(msg);
+        xiaomi.sendMsg(xiaomi.getMessage(), person);
+      } else {
+        System.out.println("See ya!");
+        break;
+      }
+      line.printSeparatorLine();
+    }
   }
 }
