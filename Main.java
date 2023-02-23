b5433cd981052f63ff7063b49b8b7758994e43c4
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-22 23:24:51 -0500

    use separator class

--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
 class Main {
   public static void main(String [] args) {
+    Separator line = new Separator();
     IOSmobile iphone = new IOSmobile();
     IOSmobile iphone2 = new IOSmobile();
+    ANDROIDmobile xiaomi = new ANDROIDmobile();
 
     System.out.println("iphone 1");
     iphone.setSize("5,4");
     iphone.setColor("red");
     System.out.println(iphone.brand+" "+iphone.getSize()+" "+ iphone.getColor());
 
+    line.printSeparatorLine();
+
     System.out.println("iphone 2");
     iphone2.setSize("6,4");
     iphone2.setColor("white");
     System.out.println(iphone.brand+" "+iphone2.getSize()+" "+ iphone2.getColor());
-  }  
+
+    line.printSeparatorLine();
+
+    System.out.println("xiaomi phone");
+    xiaomi.setBrand("xiaomi");
+    xiaomi.setSize("6,7");
+    xiaomi.setColor("purple");
+    xiaomi.setCableType("usb");
+
+    line.printSeparatorLine();
+  }
 }
