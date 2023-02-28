90b35a21d0aebde706d5e6aa8eaeb086140bcb35
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-28 10:40:21 -0500

    add overwrite methods

--- ANDROIDmobile.java
+++ ANDROIDmobile.java
@@ -2,4 +2,12 @@ class ANDROIDmobile extends Mobile {
   ANDROIDmobile() {
     super();
   }
+
+  public void turnOn() {
+    System.out.println("you have turned on your "+brand+" mobile");
+  }
+
+  public void turnOff() {
+    System.out.println("you have turned off your "+brand+" mobile");
+  }
 }
--- IOSmobile.java
+++ IOSmobile.java
@@ -3,4 +3,12 @@ class IOSmobile extends Mobile {
     super.brand = "Apple";
     super.cableType = "Lightning";
   }
+  
+  public void turnOn() {
+    System.out.println("you have turned on your "+brand+" mobile");
+  }
+
+  public void turnOff() {
+    System.out.println("you have turned off your "+brand+" mobile");
+  }
 }
